f9af08ff794d53097fac681bbc8c58de2e12f05d
Mubarak Ibrahim <elmubarak333@gmail.com>
2024-08-13 18:19:29 +0100

    update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:codeswot/features/main/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Codeswot extends StatelessWidget {
   const Codeswot({super.key});
@@ -12,14 +13,13 @@ class Codeswot extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
+        final textTheme = Theme.of(context).textTheme;
+
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Codeswot',
-          theme: ThemeData(
-            useMaterial3: true,
-            textTheme: Typography.englishLike2018.apply(
-              fontSizeFactor: 1.sp,
-            ),
+          theme: ThemeData(            
+            textTheme: GoogleFonts.nunitoTextTheme(textTheme)            
           ),
           home: child,
         );
--- lib/features/main/home.dart
+++ lib/features/main/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -9,26 +10,36 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: const Color(0xff0A192F),
-      body: Center(
-        child: RichText(
-          textAlign: TextAlign.center,
-          softWrap: true,
-          text: TextSpan(
-            text: 'Hi 👋🏾, I am ',
-            style: TextStyle(
-              fontSize: 150.sp,
-              color: const Color(0xffE6F1FF),
-            ),
-            children: [
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 32.w),
+        width: 1.sw,
+        height: 1.sh,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SelectableText.rich(
+              textAlign: TextAlign.center,  
               TextSpan(
-                text: 'Codeswot',
+                text: 'Hello World! 👋🏾, I am ',
                 style: TextStyle(
-                  fontSize: 200.sp,
-                  color: const Color(0xff64FFDA),
+                  fontSize: 150.sp,
+                  color: const Color(0xffE6F1FF),
                 ),
+                children: [
+                  TextSpan(
+                    text: 'Codeswot',
+                    style: GoogleFonts.sourceCodePro(
+                      fontSize: 200.sp,
+                      color: const Color(0xff64FFDA),
+                      letterSpacing: 0.sp,
+                      wordSpacing: 0.sp,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
