4815e61c33ddd0e0b0810f27dd8c15ecca107395
Mubarak Ibrahim <elmubarak333@gmail.com>
2024-08-14 22:19:20 +0100

    halt stone push

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:codeswot/features/main/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class Codeswot extends StatelessWidget {
   const Codeswot({super.key});
@@ -20,6 +21,15 @@ class Codeswot extends StatelessWidget {
           title: 'Codeswot',
           theme: ThemeData(            
             textTheme: GoogleFonts.nunitoTextTheme(textTheme)            
+          ),
+          builder: (context, child) => ResponsiveBreakpoints.builder(
+            child: child!,
+            breakpoints: [
+              const Breakpoint(start: 0, end: 450, name: MOBILE),
+              const Breakpoint(start: 451, end: 800, name: TABLET),
+              const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+            ],
           ),
           home: child,
         );
